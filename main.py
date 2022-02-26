4af59bf1b3c0d7420dbb8291d009c179c09be4b9
cbrandt6 <cbrandt6@ycp.edu>
2022-02-26 15:42:18 -0500

    Basic working project

---
+++ main.py
@@ -0,0 +1,19 @@
+import LCD1602
+import time
+
+
+def setup():
+    LCD1602.init(0x27, 1)  # init(slave address, background light)
+    LCD1602.write(0, 0, 'Hello')
+    LCD1602.write(1, 1, 'There')
+    LCD1602.openlight()
+    time.sleep(2)
+
+def destroy():
+	LCD1602.clear()
+
+if __name__ == "__main__":
+	try:
+		setup()
+	except KeyboardInterrupt:
+		destroy()
