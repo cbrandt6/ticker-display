3d4430422e1a3648d6473d5cb910ff88efbcdefd
cbrandt6 <cbrandt6@ycp.edu>
2022-02-26 21:50:46 -0500

    Scrolling text

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from request_helper import RequestHelper
 from lcd_helper import LCDHelper
 import time
 
-
 def main():
     lcdhelper = LCDHelper()
     request_helper = RequestHelper()
-    time.sleep(15)
+    time.sleep(2)
     lcdhelper.clear()
     lcdhelper.write("new text", (5, 0))
     lcdhelper.write("next text", (0, 1))
+    time.sleep(2)
+    lcdhelper.scrolling_write("Hello there")
+
 
 if __name__ == "__main__":
     try:
